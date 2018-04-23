e98e09329d22b47530dce60cb8820820532f7e8e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ChangeDatabase.Win/Program.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Windows.Forms;
+using ChangeDatabase.Module.Win;
+using DevExpress.ExpressApp.Security;
+
+namespace ChangeDatabase.Win {
+    static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+#if EASYTEST
+			DevExpress.ExpressApp.Win.EasyTest.EasyTestRemotingRegistration.Register();
+#endif
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            EditModelPermission.AlwaysGranted = System.Diagnostics.Debugger.IsAttached;
+            ChangeDatabaseWindowsFormsApplication winApplication = ChangeDatabaseWindowsFormsApplication.CreateApplication();
+            try {
+                DevExpress.ExpressApp.Xpo.InMemoryDataStoreProvider.Register();
+                                winApplication.ConnectionString = DevExpress.ExpressApp.Xpo.InMemoryDataStoreProvider.ConnectionString;
+                winApplication.Setup();
+                winApplication.Start();
+            }
+            catch (Exception e) {
+                winApplication.HandleException(e);
+            }
+            winApplication.Dispose();
+        }
+    }
+}
---
+++ VB/ChangeDatabase.Module/Updater.vb
@@ -0,0 +1,40 @@
+Imports Microsoft.VisualBasic
+Imports DevExpress.ExpressApp
+Imports System
+Imports DevExpress.Xpo
+Imports DevExpress.Data.Filtering
+Imports DevExpress.Persistent.BaseImpl
+Imports DevExpress.ExpressApp.Updating
+Imports DevExpress.ExpressApp.Xpo
+
+Namespace ChangeDatabase.Module
+	Public Class Updater
+		Inherits ModuleUpdater
+		Public Sub New(ByVal objectSpace As IObjectSpace, ByVal currentDBVersion As Version)
+			MyBase.New(objectSpace, currentDBVersion)
+		End Sub
+		Public Overrides Sub UpdateDatabaseAfterUpdateSchema()
+			MyBase.UpdateDatabaseAfterUpdateSchema()
+			If ObjectSpace.FindObject(Of SimpleUser)(New BinaryOperator("UserName", "Admin")) Is Nothing Then
+				Dim user As SimpleUser = ObjectSpace.CreateObject(Of SimpleUser)()
+				user.UserName = "Admin"
+				user.Save()
+			End If
+
+            If CType(ObjectSpace, XPObjectSpace).Session.ConnectionString.Contains("DB1") Then
+                If ObjectSpace.FindObject(Of SimpleUser)(New BinaryOperator("UserName", "Admin1")) Is Nothing Then
+                    Dim user1 As SimpleUser = ObjectSpace.CreateObject(Of SimpleUser)()
+                    user1.UserName = "Admin1"
+                    user1.Save()
+                End If
+            End If
+            If CType(ObjectSpace, XPObjectSpace).Session.ConnectionString.Contains("DB2") Then
+                If ObjectSpace.FindObject(Of SimpleUser)(New BinaryOperator("UserName", "Admin2")) Is Nothing Then
+                    Dim user2 As SimpleUser = ObjectSpace.CreateObject(Of SimpleUser)()
+                    user2.UserName = "Admin2"
+                    user2.Save()
+                End If
+            End If
+		End Sub
+	End Class
+End Namespace
